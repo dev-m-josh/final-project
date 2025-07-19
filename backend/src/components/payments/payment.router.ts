acda113d2b5154901abc83e55f923b4d72487404
dev-m-joshua <mutambukijoshua2@gmail.com>
2025-07-19 16:51:26 +0300

    Payment routes change

--- backend/src/components/payments/payment.router.ts
+++ backend/src/components/payments/payment.router.ts
@@ -5,10 +5,10 @@ import * as paymentsController from "./payment.controller";
 const paymentRouter = express.Router();
 
 paymentRouter.get("/", paymentsController.getAllPayments);
-paymentRouter.get("/:id", paymentsController.getPaymentById as any);
-paymentRouter.post("/", paymentsController.createPayment);
-paymentRouter.put("/:id", paymentsController.updatePayment as any);
-paymentRouter.delete("/:id", paymentsController.deletePayment as any);
+paymentRouter.get("/details/:id", paymentsController.getPaymentById as any);
+paymentRouter.post("/add", paymentsController.createPayment);
+paymentRouter.put("/update/:id", paymentsController.updatePayment as any);
+paymentRouter.delete("/delete/:id", paymentsController.deletePayment as any);
 paymentRouter.get("/user/:userId", paymentsController.getPaymentsByUserId as any);
 
 export default paymentRouter;

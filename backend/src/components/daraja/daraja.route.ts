4ad35e776ebc20aa8cb1ffd1ba9bb4a019635dab
dev-m-joshua <mutambukijoshua2@gmail.com>
2025-07-22 11:31:22 +0300

    route error

--- backend/src/components/daraja/daraja.route.ts
+++ backend/src/components/daraja/daraja.route.ts
@@ -1,9 +1,16 @@
-import express from "express";
+import express, { Request, Response } from "express";
+
 import { lipaNaMpesaOnline } from "./daraja.controller";
 
 const darajaRouter = express.Router();
 
 // POST /api/daraja/stkpush
 darajaRouter.post("/stkpush", lipaNaMpesaOnline as any);
+
+// Daraja callback URL endpoint
+darajaRouter.post("/payments/callback", (req: Request, res: Response) => {
+  console.log("Callback received from Daraja:", req.body);
+  res.status(200).send("Callback received");
+});
 
 export default darajaRouter;
